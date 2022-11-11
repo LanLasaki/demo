0207a3950eb41afd8e677ccfc134c484d7319848
LanLasaki <64391473+LanLasaki@users.noreply.github.com>
2022-11-10 22:47:21 -0500

    added getter and setter for publisher

--- src/main/java/com/example/demo/Model/Book.java
+++ src/main/java/com/example/demo/Model/Book.java
@@ -26,6 +26,14 @@ public class Book {
         this.publisher = publisher;
     }
 
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
+    }
+
     public Book() {
     }
 
